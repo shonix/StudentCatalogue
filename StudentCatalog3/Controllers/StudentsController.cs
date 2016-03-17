184adcc378e2504c743acd039e6d1df946624eaf
Peter Raasthoej <peterfut123@gmail.com>
2016-03-17 01:43:02 +0100

    initial

---
+++ StudentCatalog3/Controllers/StudentsController.cs
@@ -0,0 +1,80 @@
+﻿using StudentCatalog3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentCatalog2.Controllers
+{
+    public class StudentsController : Controller
+    {
+        //quick - and dirty - D.I. later
+        ApplicationDbContext db =
+                    new ApplicationDbContext();
+
+        // GET: Students
+        //action method
+        public ActionResult Index()
+        {
+            List<Student> students = 
+                db.Students.ToList();
+
+            return View(students);
+        }
+
+        public string WannaPlayDad()
+        {
+            return "No, I am coding..";
+        }
+
+        public ActionResult MadView()
+        {
+            //defining Cat property
+            ViewBag.Cat = "Miauu, I am a cat";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            //Find the student from the id 
+            //and send to view.
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            //set it to update the student object when you save
+            db.Entry(student).State = 
+                EntityState.Modified;
+            
+            //go back to index-view after succesful edit.
+
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            //if no broken rules, defined in student
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                return View("Thanks");
+            }
+            return View();
+
+        }
+    }
+}
---
+++ StudentCatalog3/Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentCatalog3.Models
+{
+    public class Student
+    {
+        //C#
+        public int StudentId { get; set; }
+        [Required]
+        public string Firstname { get; set; }
+
+        [Required]
+        public string Lastname { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Stupid user. Must fill out this")]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string MobilePhone { get; set; }
+
+
+        //java
+        //private string firstname;
+        //public string getFirstname()
+        //{
+        //    return firstname;
+        //}
+        //public void setFirstname(string firstname)
+        //{
+        //    this.firstname = firstname;
+        //}
+    }
+}
